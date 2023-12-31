3ebb50fd655f08a74ced7dbf34a0547c21ab7e40
Ata <ata.khatibi@gmail.com>
2024-01-01 00:28:37 +0330

    Second Commit for 'Set' Data Type

--- set_demo.py
+++ set_demo.py
@@ -1,4 +1,5 @@
 # unordered data type
+# mutable
 emptySet =set()
 sampleSet = {1,'A','g', 3.86, 'Ata'}
 print(sampleSet)
@@ -6,11 +7,21 @@ print(sampleSet)
 for _ in range(10):
     print(sampleSet)
 
-print('=' *40)
+
+print('=' *60)
 
 odd = set(range(1,50,2))
+print(odd)
 power = {1,4,9,16,25,36,49}
 print(len(odd))
 print(odd.intersection(power))
 print(odd.union(power))
 print(len(odd.union(power)))
+
+print('=' *40)
+print(odd.intersection_update(power))
+print(odd)
+
+power_frozen = frozenset(power)
+
+
